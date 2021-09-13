32997aa9b5fc5d90094dff9333d4a32c72568ad8
OoMorizono <x65f588d72sw76@gmail.com>
2021-09-13 23:38:35 +0900

    fix

--- exercise/01/array_2.php
+++ exercise/01/array_2.php
@@ -1,7 +1,5 @@
 <?php
 
-use PhpParser\Node\Stmt\Echo_;
-
 $coffee = ['カフェオレ','カフェラテ','カプチーノ'];
 
 echo var_dump($coffee);
--- exercise/02/if_4.php
+++ exercise/02/if_4.php
@@ -4,7 +4,7 @@ $score_english = 24;
 
 if ($score_math >= 60 && $score_english >= 60) {
     echo '合格';
-} elseif ($score_math >= 30 && $score_math <= 60 || $score_english >= 30 && $score_english <= 60) {
+} elseif ($score_math >= 30 || $score_english >= 30 ) {
     echo '再試験';
 } else {
     echo '不合格';
